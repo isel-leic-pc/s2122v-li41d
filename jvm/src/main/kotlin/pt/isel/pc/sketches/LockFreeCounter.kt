d9d910ad3925b8621a30d08fc9da21661d4c3f2e
Pedro Felix <pmhsfelix@gmail.com>
2022-05-02 07:56:26 +0200

    adds lock free counter using CAS

---
+++ jvm/src/main/kotlin/pt/isel/pc/sketches/LockFreeCounter.kt
@@ -0,0 +1,44 @@
+package pt.isel.pc.sketches
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class LockFreeCounter {
+
+    private val counter = AtomicInteger(0)
+
+    fun inc() {
+        while(true) {
+            val observed = counter.get()
+            val nextValue = observed + 1
+            if (counter.compareAndSet(observed, nextValue)) {
+                return
+            }
+            // retry
+        }
+    }
+
+    fun dec() {
+        while(true) {
+            val observed = counter.get()
+            val nextValue = observed - 1
+            if(counter.compareAndSet(observed, nextValue)) {
+                return
+            }
+            // retry
+        }
+    }
+
+    
+
+    fun wrongIncrement() {
+        while(true) {
+            if (counter.compareAndSet(counter.get(), counter.get() + 1)) {
+                return;
+            }
+            // retry
+        }
+    }
+
+
+
+}
